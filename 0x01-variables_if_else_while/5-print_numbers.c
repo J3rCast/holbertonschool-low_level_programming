81f44fec4eb27245294a29e390ab9834e988129c
J3rCast <jerson.castro.r@gmail.com>
2022-02-11 14:56:35 -0500

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - this program prints numbers
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	putchar('\n');
+
+	return (0);
+}
