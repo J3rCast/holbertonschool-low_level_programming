d62d66e63ae180ea47b96d24f4af1c520973945c
J3rCast <jerson.castro.r@gmail.com>
2022-02-14 12:08:16 -0500

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - this program prints putchar using putchar function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alfa;
+	alfa = 'a';
+
+	while (alfa <= 'z')
+	{
+		_putchar(alfa);
+		alfa++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
