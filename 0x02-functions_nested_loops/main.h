2a923c17c02f34ebc71b86d2462a12e4a75ebe3a
J3rCast <jerson.castro.r@gmail.com>
2022-02-15 06:08:24 -0500

    update

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,8 +7,9 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-/*int _putchar(char c)
-{
+/*int _putchar(char c)*/
+int _putchar(char);
+/*{
 	return (write(1, &c, 1));
 }*/
 /*this function prints alphabet*/
