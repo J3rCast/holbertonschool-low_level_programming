e2d54ff6e0acab02ff615ca8c1026f2e4516b44c
J3rCast <jerson.castro.r@gmail.com>
2022-02-15 08:24:24 -0500

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,42 @@
 #include "main.h"
 /**
- * main - this program prints laphabet follow by a new line
+ * jack_bauer - this program prints laphabet follow by a new line
  *
- * Return: 0
+ * Return: minutes
  */
 
-int main(void)
+void jack_bauer(void)
 {
-	void jack_bauer(void);
-	return (0);
+	char a = '0';
+	char b = '0';
+	char c = '0';
+	char d = '0';
+
+	while (a <= '2')
+	{
+		while (b <= '3')
+		{
+			while (c <= '5')
+			{
+				while (d <= '9')
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(':');
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+					d++;
+				}
+			d = '0';
+			c++;
+			}
+		c = '0';
+		d = '0';
+		b++;
+		}
+	b = '0';
+	d = '0';
+	a++;
+	}
 }
