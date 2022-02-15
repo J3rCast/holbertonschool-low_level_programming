85a18ef79204bed95ef59f4499941df44091d31e
J3rCast <jerson.castro.r@gmail.com>
2022-02-15 16:04:15 -0500

    update natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - thi program prints every minute
+ *
+ * @n: integer value
+ *
+ * Return: minutes
+ */
+void main(void)
+{
+	int n = 0;
+	int sum = 0;
+
+	while (n < 1024)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+		{
+			sum += n;
+			printf("%d\n", sum);
+		}
+		n++;
+	}
+}
