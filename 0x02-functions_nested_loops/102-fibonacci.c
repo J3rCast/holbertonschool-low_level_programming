906f10a138913e50754cdbbd12d35c7adc97b250
J3rCast <jerson.castro.r@gmail.com>
2022-02-15 17:26:35 -0500

    update fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - this program prints every minute
+ *
+ * Return: sum
+ */
+int main(void)
+{
+	int n = 0;
+	int m = 1;
+	int sum = 0;
+
+	while (n <= 50)
+	{
+		sum += m;
+		printf("%d, " sum);
+		m++;
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
