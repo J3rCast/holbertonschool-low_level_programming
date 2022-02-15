7bd79de444ec1d2e35331158a2ffd346b626326f
J3rCast <jerson.castro.r@gmail.com>
2022-02-15 14:20:22 -0500

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,17 +13,19 @@ void print_to_98(int n)
 	{
 		while(n <= 98)
 		{
-			printf("%d\n, ", n);
+			printf("%d, ", n);
 			n++;
 		}
+		printf("\n");
 	}
 	else if (n > 98)
 	{
 		while(n >= 98)
 		{
-			printf("%d\n, ", n);
+			printf("%d, ", n);
 			n--;
 		}
+		printf("\n");
 	}
 	else
 	{
