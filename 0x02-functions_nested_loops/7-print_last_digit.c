a29918478ddbea718e37cd98c2d3511dfa117359
J3rCast <jerson.castro.r@gmail.com>
2022-02-15 12:56:03 -0500

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,14 +2,15 @@
 /**
  * print_last_digit - this program prints last digit of a number
  *
- * @num: character value
+ * @num: number value
  *
  * Return: num
  */
 
-int print_last_digit(num)
+int print_last_digit(int num)
 {
 	int lastDigit;
-	lastDigit = (1 * num) % 10;
+
+	lastDigit = ((1 * num) % 10);
 	return (lastDigit);
 }
