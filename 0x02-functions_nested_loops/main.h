e39993eb3e0a4dc189b79c224c2164fa4827564f
J3rCast <jerson.castro.r@gmail.com>
2022-02-15 10:04:11 -0500

    update main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -24,40 +24,9 @@ int _isalpha(int c);
 
 /*this funtion prints the sign of a number*/
 int print_sign(int n);
-/*{
-	if (n < 0)
-	{
-		_putchar('-');
-		return(-1);
-	}
-	else if (n > 0)
-	{
-		_putchar('+');
-		return(1);
-	}
-	else
-	{
-		_putchar('0');
-		return(0);
-	}
-}*/
 
 /*this fuctio coputes the absolute value of an integer*/
 int _abs(int num);
-/*{
-	if (num < 0)
-	{
-		num = num * -1;
-		return(num);
-	}
-	else
-	{
-		num = num * 1;
-		return(num);
-	}
-}*/
-
-/*this fuction prints the last digit of a number*/
 
 /*REMINDER, DOESNT WORK*/
 int print_last_digit(int num);
@@ -70,37 +39,6 @@ int print_last_digit(int num);
 
 /*this function prints every minute of the day of Jack Bauer*/
 void jack_bauer(void);
-/*{
-	char a = '0';
-	char b = '0';
-	char c = '0';
-	char d = '0';
 
-	while (a <= '2')
-	{
-		while (b <= '3')
-		{
-			while (c <= '5')
-			{
-				while (d <= '9')
-				{
-					_putchar(a);
-					_putchar(b);
-					_putchar(':');
-					_putchar(c);
-					_putchar(d);
-					_putchar('\n');
-					d++;
-				}
-			d = '0';
-			c++;
-			}
-		c = '0';
-		d = '0';
-		b++;
-		}
-	b = '0';
-	d = '0';
-	a++;
-	}
-}*/
+/*this function prints a nine time table*/
+void times_table(void);
