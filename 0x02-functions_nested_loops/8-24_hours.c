d36d4469ff2872fd3a56d49e796ffca26c16d7f4
J3rCast <jerson.castro.r@gmail.com>
2022-02-15 09:43:19 -0500

    update 24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,15 +10,11 @@ void jack_bauer(void)
 	char b = '0';
 	char c = '0';
 	char d = '0';
-	char num;
+	char num = '9';
 
 	while (a <= '2')
 	{
-		if (a < '2')
-		{
-			num = '9';
-		}
-		else
+		if (a = '2')
 		{
 			num = '3';
 		}
