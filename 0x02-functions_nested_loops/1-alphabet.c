1ac660340b4836b826263d054b7cbe21bd2664a7
J3rCast <jerson.castro.r@gmail.com>
2022-02-15 06:37:58 -0500

    update alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,13 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * main - this program prints putchar using putchar function
+ * main - this program prints alphabet
  *
  * Return: 0
  */
 
-void print_alphabet(void)
+int main(void)
 {
-	char alfa = 'a';
-
-	while (alfa <= 'z')
-	{
-		write(1, &alfa, 1);
-		alfa++;
-	}
-	write(1, "\n", 1);
+	print_alphabet();
+	return (0);
 }
