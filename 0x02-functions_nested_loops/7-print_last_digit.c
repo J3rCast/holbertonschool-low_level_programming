b00ba662a12884bdce67a0beacf93ba4101269bd
J3rCast <jerson.castro.r@gmail.com>
2022-02-15 15:07:29 -0500

    update print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -15,8 +15,8 @@ int print_last_digit(int n)
 	{
 		last_digit *= -1;
 		_putchar(last_digit);
-		return(last_digit);
 	}
 	return(last_digit);
 	_putchar(last_digit);
+	_putchar('0' + last_digit);
 }
