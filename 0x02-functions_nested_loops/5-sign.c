1d4903674950ec2bbdbbea6b9c8631e501f660a0
J3rCast <jerson.castro.r@gmail.com>
2022-02-15 08:00:49 -0500

    update main

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * aprint_sign - this program prints the sign of the character
+ * print_sign - this program prints the sign of the character
  *
  * @n: character value
  *
@@ -11,16 +11,16 @@ int print_sign(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		return(-1);
+		return (-1);
 	}
 	else if (n > 0)
 	{
 		_putchar('+');
-		return(1);
+		return (1);
 	}
 	else
 	{
 		_putchar('0');
-		return(0);
+		return (0);
 	}
 }
