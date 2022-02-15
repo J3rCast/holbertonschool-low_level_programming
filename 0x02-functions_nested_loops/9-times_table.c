e0a7b5524f320496bfda67e557aeb088bb3c7b04
J3rCast <jerson.castro.r@gmail.com>
2022-02-15 10:05:42 -0500

    9  times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,9 +10,10 @@
 void times_table(void)
 {
 	int n = 0;
+	int num;
   while (n <= 9)
 	{
-		for (int num = 0; num <= 9; num++)
+		for (num = 0; num <= 9; num++)
 		{
 			int x = num * n;
 			_putchar(x);
