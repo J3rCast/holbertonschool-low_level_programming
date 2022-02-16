aa8102bbe76f4dd1ec086b01f924555031b94fa7
J3rCast <jerson.castro.r@gmail.com>
2022-02-16 15:06:00 -0500

    update print remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -31,10 +31,10 @@ void print_remaining_days(int month, int day, int year)
 	{
 		/*else*/
 		/*{*/
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		/*}*/
 	}
-	else 
+	else
 	{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
