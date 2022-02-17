43f2279a082a4b6912c5cf1424753954fe36b6b6
J3rCast <jerson.castro.r@gmail.com>
2022-02-17 15:55:34 -0500

    print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_triangle - this function multiplies two values
+ *
+ * @size: value
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = i; j < size; j++)
+		{
+			_putchar(" ");
+		}
+        	for (k = 1; k <= i; k++)
+        	{
+            		_putchar("#");
+        	}
+        	_putchar("\n");
+    	}
+}
