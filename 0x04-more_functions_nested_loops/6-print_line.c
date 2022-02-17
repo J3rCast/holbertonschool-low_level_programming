07f166be4997d7e7810e14d741872842988ac1d0
J3rCast <jerson.castro.r@gmail.com>
2022-02-17 10:53:50 -0500

    update print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - this function multiplies two values
+ *
+ * @n: value
+ *
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int n;
+	int m;
+
+	for (m = 0; m < n; m++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
