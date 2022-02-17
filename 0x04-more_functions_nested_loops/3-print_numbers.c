f06fa691077873f5eafac37db269157db09d6523
J3rCast <jerson.castro.r@gmail.com>
2022-02-17 09:31:47 -0500

    update print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - this function multiplies two values
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar('0' + n);
+		_putchar('\n');
+	}
+}
