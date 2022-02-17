65592b437e76b091fd75fceb5e0581dbc09ea2c0
J3rCast <jerson.castro.r@gmail.com>
2022-02-17 10:03:30 -0500

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,13 +3,14 @@
  * print_most_numbers - this function multiplies two values
  *
  */
+
 void print_most_numbers(void)
 {
 	int n;
 
 	for (n = 0; n <= 9; n++)
 	{
-		if (n != 2 || n != 4)
+		if ((n != 2) && (n != 4))
 		{
 			_putchar('0' + n);
 		}
