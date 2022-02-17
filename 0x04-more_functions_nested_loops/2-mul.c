e5e4e6a9adab7f0d3b9e100e1c8195a6587036ec
J3rCast <jerson.castro.r@gmail.com>
2022-02-17 09:25:02 -0500

    update mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isdigit - this function  identifies if a character si a digit or not
+ *
+ * @c: character value
+ *
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int result;
+	result = a * b;
+
+	_putchar('0' + result);
+}
