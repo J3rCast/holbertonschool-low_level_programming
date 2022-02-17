02c69adeea141dd55d6bbe6907c5170a450ae8cb
J3rCast <jerson.castro.r@gmail.com>
2022-02-17 09:15:00 -0500

    update is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,14 @@
 #include "main.h"
 /**
+ * _isupper - this function  identifies an uppercase letter
  *
+ * @c: character value
+ *
+ * Return: 0
  */
 int _isupper(int c)
 {
-	if(c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
