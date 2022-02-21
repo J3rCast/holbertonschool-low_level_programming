3423f230a37a0c14a0229bae7c19bc691a0f2625
J3rCast <jerson.castro.r@gmail.com>
2022-02-21 13:14:42 -0500

    update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,3 +2,5 @@
 int _putchar(char c);
 /*this function change the value it points to 98*/
 void reset_to_98(int *n);
+ /*this function swaps the value of two integers*/
+void swap_int(int *a, int *b);
