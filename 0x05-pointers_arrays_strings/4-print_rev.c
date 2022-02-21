991f1744588442bc983b2045ae0e93d1068151fe
J3rCast <jerson.castro.r@gmail.com>
2022-02-21 15:07:23 -0500

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strlen - this function return the legth of a string
+ *
+ * @s: value
+ *
+ * Return: value
+ */
+void print_rev(char *s)
+{
+	char n;
+	int i;
+
+	for (i = 0; (n != '\0'); i++)
+	{
+		n = s[i];
+	}
+	for (p = (i - 1); p = 0; p--)
+	{
+		n = s[p];
+		_putchar(n);
+	}
+	return (i - 1);
+}
