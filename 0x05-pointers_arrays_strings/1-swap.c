59351c782c3acda82de0c92f0e7fa8661982d742
J3rCast <jerson.castro.r@gmail.com>
2022-02-21 13:21:21 -0500

    update swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,8 +4,16 @@
  * @a: value
  * @b: value
  */
+void swap_a(int *num1)
+{
+	*num1 = *b;
+}
+void swap_b(int *num2)
+{
+	*num2 = *a;
+}
 void swap_int(int *a, int *b)
 {
-	*a = *b;
-	*b = *a;
+	void swap_a(*a);
+	void swap_b(*b);
 }
