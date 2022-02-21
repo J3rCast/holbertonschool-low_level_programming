495482795bbae4f8741eaab328053899c08aa1b4
J3rCast <jerson.castro.r@gmail.com>
2022-02-21 13:52:56 -0500

    update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,3 +4,5 @@ int _putchar(char c);
 void reset_to_98(int *n);
  /*this function swaps the value of two integers*/
 void swap_int(int *a, int *b);
+/*this function returns the length of a string*/
+int _strlen(char *s);
