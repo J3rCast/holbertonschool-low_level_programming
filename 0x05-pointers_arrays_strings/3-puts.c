e39231b726f7651b9c290f96795f8c11ca2c9843
J3rCast <jerson.castro.r@gmail.com>
2022-02-21 14:40:36 -0500

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strlen - this function return the legth of a string
+ *
+ * @s: value
+ *
+ * Return: value
+ */
+void _puts(char *str)
+{
+	char n;
+	int i;
+
+	for (i = 0; (n != '\0'); i++)
+	{
+		n = str[i];
+		_putchar(n);
+	}
+	_putchar('\n');
+}
