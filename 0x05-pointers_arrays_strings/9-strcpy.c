7bb2e91abd76ae5d293cae2d92805313711783d0
J3rCast <jerson.castro.r@gmail.com>
2022-02-22 09:11:38 -0500

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strcpy - this function return the legth of a string
+ *
+ * @dest: where the source need to be copied
+ * @src: source of the string
+ *
+ * Return: Dest ponter
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int lenth = 0;
+	
+	while (src[length] ¡= '\0')
+	{
+		src[length] = dest[lenth];
+		length++;
+	}
+	return (*dest);
+}
