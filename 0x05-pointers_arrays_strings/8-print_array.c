f63194e861873659f5a58b55d598aff8c354c000
J3rCast <jerson.castro.r@gmail.com>
2022-02-21 19:46:09 -0500

    update print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,20 +11,19 @@ void print_array(int *a, int n)
 {
 	int i;
 
+	if (n <= 0)
+	{
+			printf("\n");
+	}
 	for (i = 0; i < n; i++)
 	{
 		if ((i + 1) != n)
 		{
 			printf("%d, ", a[i]);
 		}
-		else if ((i + 1) == n)
-		{
-			printf("%d", a[i]);
-			printf("\n");
-		}
 		else
 		{
-			printf("\n");
+			printf("%d", a[i]);
 			printf("\n");
 		}
 	}
