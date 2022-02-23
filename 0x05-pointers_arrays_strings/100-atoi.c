524ee5ba6914f0383220d9c6466593d157088c92
J3rCast <jerson.castro.r@gmail.com>
2022-02-23 11:28:28 -0500

    atoi}

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,13 +16,13 @@ int _atoi(char *s)
 	{
 		if (s[i] == '-' && s[i + 1] > '0' && s[i + 1] <= '9')
 		{
-			return (s[i]);
+			_putchar(s[i]);
 		}
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			return (s[i]);
+			_putchar(s[i]);
 		}
-		if (s[i] > '9' || s[i] < '0')
+		else if (s[i] > '9' || s[i] < '0')
 		{
 			return (0);
 		}
