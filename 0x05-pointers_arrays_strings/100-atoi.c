82791f89f9d6620627bb063df2726deec39fce4f
J3rCast <jerson.castro.r@gmail.com>
2022-02-23 11:26:57 -0500

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,11 +16,11 @@ int _atoi(char *s)
 	{
 		if (s[i] == '-' && s[i + 1] > '0' && s[i + 1] <= '9')
 		{
-			return (s[i] + 0);
+			return (s[i]);
 		}
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			return (s[i] + 0);
+			return (s[i]);
 		}
 		if (s[i] > '9' || s[i] < '0')
 		{
