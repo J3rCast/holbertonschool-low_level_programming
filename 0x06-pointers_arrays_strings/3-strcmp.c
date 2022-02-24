07196910087da3ff83d43337dac1cb50f57ecc2e
J3rCast <jerson.castro.r@gmail.com>
2022-02-24 15:54:56 -0500

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,12 +14,12 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[i] != '\0' || s2[i] != '\0')
 	{
-		if (s1[i] > s2[i])
+		if (s1[i] < s2[i])
 		{
 			result = 15;
 			break;
 		}
-		else if (s1[i] < s2[i])
+		else if (s1[i] > s2[i])
 		{
 			result = -15;
 			break;
