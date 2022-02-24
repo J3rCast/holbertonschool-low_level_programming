cba53f300f38396f776da5d43f106057b950fcd1
J3rCast <jerson.castro.r@gmail.com>
2022-02-24 17:33:42 -0500

    string to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,15 +9,13 @@
  */
 char *string_toupper(char *string)
 {
+	int i;
+
 	for (i = 0; string[i] != '\0'; i++)
 	{
 		if (string[i] >= 'a' && <= 'z')
 		{
 			return (string[i] + 32);
-		}
-		else
-		{
-			return (string[i]);
 		}
 	}
 }
