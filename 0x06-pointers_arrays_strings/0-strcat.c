2d0aeccb4b9344e185195160d6e7237fc7be3dc1
J3rCast <jerson.castro.r@gmail.com>
2022-02-24 11:42:44 -0500

    update strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,55 @@
+#include "main.h"
+/**
+ * *_strcat - this function concatnates two strings
+ */
+char *_strcat(char *dest, char *src)
+{
+	int length = 0;
+	int length2 = 0;
+	char complete[98];
+
+	while(dest[length] != '\0')
+	{
+		complete[length] = dest[length];
+		length++;
+		if (dest[length] == '\0') 
+		{
+			while(src[length2] != '\0')
+			{
+				complete[length] = src[length2];
+				length++;
+				length2++;
+			}
+		}
+	}
+	int length = 0;
+	int length2 = 0;
+	char complete[98];
+	char dest[] = "hola ";
+	char src[] = "mundo";
+
+	while(dest[length] != '\0')
+	{
+		if (dest[length2] != '\0')
+		{
+			complete[length] = dest[length];
+		}
+		length++;
+		if (dest[length] == '\0')
+		{
+			while(src[length2] != '\0')
+			{
+				complete[length] = src[length2];
+				length++;
+				length2++;
+			}
+		}
+	}
+	length = 0;
+	while (complete[length != '\0'])
+	{
+		dest[length] = complete[length];
+		length++;
+	}
+	return (dest);
+}
