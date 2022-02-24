4fe9918d5a90c3f2a6230d2513b7614c7c4b6542
J3rCast <jerson.castro.r@gmail.com>
2022-02-24 14:58:14 -0500

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,12 +16,12 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (s1[i] > s2[i])
 		{
-			result = 1;
+			result = 15;
 			break;
 		}
 		else if (s1[i] < s2[i])
 		{
-			result = -1;
+			result = -15;
 			break;
 		}
 		else if (s1[i] == s2[i])
