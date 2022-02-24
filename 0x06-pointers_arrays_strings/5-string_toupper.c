e30dfe8864ec6001e3192dd345c4ac6630c9829d
J3rCast <jerson.castro.r@gmail.com>
2022-02-24 17:27:14 -0500

    string to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _toupper - this function pass to upper
+ *
+ * @s1: dest string
+ * @s2: src string
+ *
+ * Return: 0
+ */
+char *string_toupper(char *string)
+{
+	for (i = 0; string[i] != '\0'; i++)
+	{
+		if (string[i] >= 'a' && <= 'z')
+		{
+			string[i] = string[i + 32];
+		}
+	}
+}
