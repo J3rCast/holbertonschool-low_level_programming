67182155280911d46cae28ee0639acc10f627e3f
J3rCast <jerson.castro.r@gmail.com>
2022-02-25 10:28:47 -0500

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,17 +12,17 @@ int _strcmp(char *s1, char *s2)
 	int result = 0;
 	int i = 0;
 
-	while((*s1[i] != '\0' && *s2[i] != '\0') && *s1[i] == *s2[i])
+	while((s1[i] != '\0' && s2[i] != '\0') && s1[i] == s2[i])
 	{
 		i++;
 	}
-	if(*s1[i] == *s2[i])
+	if(s1[i] == s2[i])
 	{
 		result = 0; // strings are identical
 	}
 	else
 	{
-		result = *s1[i] - *s2[i];
+		result = s1[i] - s2[i];
 	}
 	return (result);
 }
