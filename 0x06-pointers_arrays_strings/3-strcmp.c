247305b5b58de0d9fd3d48c8b01c9d421da67822
J3rCast <jerson.castro.r@gmail.com>
2022-02-25 13:48:35 -0500

    update strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,7 @@
  * @s1: dest string
  * @s2: src string
  *
- * Return: 0
+ * Return: result
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -16,7 +16,7 @@ int _strcmp(char *s1, char *s2)
 	{
 		i++;
 	}
-	if(s1[i] == s2[i])
+	if (s1[i] == s2[i])
 		result = 0;
 	else
 		result = s1[i] - s2[i];
