a2a9f0820a3bb8b07e9b90a84581f8946f5ee28b
J3rCast <jerson.castro.r@gmail.com>
2022-02-25 13:11:54 -0500

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strcmp - this function concatnates two strings
+ * _strcmp - this function compares two strings
  *
  * @s1: dest string
  * @s2: src string
@@ -9,7 +9,7 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int result = 0;
+	int result = -1;
 	int i = 0;
 
 	while((s1[i] != '\0' && s2[i] != '\0') && s1[i] == s2[i])
