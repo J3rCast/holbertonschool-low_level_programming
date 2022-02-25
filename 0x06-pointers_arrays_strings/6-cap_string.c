8b77933d280d8aaeaf2f83d07714613e029cd6fa
J3rCast <jerson.castro.r@gmail.com>
2022-02-25 11:37:19 -0500

    cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - this function concatnates two strings
+ *
+ * @s1: dest string
+ * @s2: src string
+ *
+ * Return: 0
+ */
+char *cap_string(char *s)
+{
+	int i;
+	int tmp;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i - 1] == " ")
+		{
+			return = s[i + 32];
+		}
+	}
+}
