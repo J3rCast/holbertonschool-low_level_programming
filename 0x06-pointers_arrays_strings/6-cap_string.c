5c050d1d3854d02f3287bd896fa2cf72cbfa9d40
J3rCast <jerson.castro.r@gmail.com>
2022-02-25 12:13:37 -0500

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,37 +10,36 @@
 char *cap_string(char *s)
 {
 	int i;
-	int tmp;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[0] >= 'a' && s[0] <= 'z')
-			return (s[i + 1] -= 32);
+		if (s[i] == s[0] && s[i] >= 'a' && s[0] <= 'z')
+			s[i + 1] -= 32;
 		if (s[i - 1] == ' ' && s[i] >= 'a' && s[i] <= 'z')
-			return (s[i + 1] -= 32);
+			s[i + 1] -= 32;
 		if (s[i - 1] == ',' && s[i] >= 'a' && s[i] <= 'z')
-			return (s[i + 1] -= 32);
+			s[i + 1] -= 32;
 		if (s[i - 1] == ';' && s[i] >= 'a' && s[i] <= 'z')
-			return (s[i + 1] -= 32);
+			s[i + 1] -= 32;
 		if (s[i - 1] == '.' && s[i] >= 'a' && s[i] <= 'z')
-			return (s[i + 1] -= 32);
+			s[i + 1] -= 32;
 		if (s[i - 1] == '!' && s[i] >= 'a' && s[i] <= 'z')
-			return (s[i + 1] -= 32);
+			s[i + 1] -= 32;
 		if (s[i - 1] == '?' && s[i] >= 'a' && s[i] <= 'z')
-			return (s[i + 1] -= 32);
+			s[i + 1] -= 32;
 		if (s[i - 1] == '\"' && s[i] >= 'a' && s[i] <= 'z')
-			return (s[i + 1] -= 32);
+			s[i + 1] -= 32;
 		if (s[i - 1] == '(' && s[i] >= 'a' && s[i] <= 'z')
-			return (s[i + 1] -= 32);
+			s[i + 1] -= 32;
 		if (s[i - 1] == ')' && s[i] >= 'a' && s[i] <= 'z')
-			return (s[i + 1] -= 32);
+			s[i + 1] -= 32;
 		if (s[i - 1] == '{' && s[i] >= 'a' && s[i] <= 'z')
-			return (s[i + 1] -= 32);
+			s[i + 1] -= 32;
 		if (s[i - 1] == '}' && s[i] >= 'a' && s[i] <= 'z')
-			return (s[i + 1] -= 32);
+			s[i + 1] -= 32;
 		if (s[i - 1] == '\n' && s[i] >= 'a' && s[i] <= 'z')
-			return (s[i + 1] -= 32);
+			s[i + 1] -= 32;
 		if (s[i - 1] == '\t' && s[i] >= 'a' && s[i] <= 'z')
-			return (s[i + 1] -= 32);
+			s[i + 1] -= 32;
 	}
 }
