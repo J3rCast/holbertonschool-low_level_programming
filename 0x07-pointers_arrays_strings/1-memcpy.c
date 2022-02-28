cbaece8fb740236887f703935eec9d1b56842c4e
J3rCast <jerson.castro.r@gmail.com>
2022-02-28 14:12:29 -0500

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - this function copies memory area
+ *
+ * @dest: where the src will get copied
+ * @src: area to copy
+ * @n: copies n bytes of memory area
+ *
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	while (n != 0)
+	{
+		dest++ == src++;
+	}
+	return (dest);
+}
