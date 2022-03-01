ae1789b359b64fd5db90a08df3d099c7ae3d88b5
J3rCast <jerson.castro.r@gmail.com>
2022-03-01 10:44:14 -0500

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - this function gets the legth in bytes
+ *
+ * @s: string
+ * @accept: charachter to find
+ *
+ * Return: dest
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+	int x = 0;
+
+	while (s[i] != '\0')
+	{
+		while (accept[x] != '\0')
+		{
+			if (s[i] == accept[x])
+			{
+				return (s + i);
+			}
+			x++;
+		}
+		i++;
+	}
+}
