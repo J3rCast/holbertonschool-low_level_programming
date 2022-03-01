b2a7d4589e4b997845f8a7c1090f8bd799ea63cc
J3rCast <jerson.castro.r@gmail.com>
2022-03-01 16:08:24 -0500

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -25,8 +25,8 @@ char *_strstr(char *haystack, char *needle)
 		}
 		if (needle[j] == 0)
 		{
-			return [haystack + i];
+			return (haystack + i);
 		}
 	}
-	return (0);
+	return (haystack + i);
 }
