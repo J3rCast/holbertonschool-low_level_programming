2f2814e83360359f3f69ccee4805446be24a78c5
J3rCast <jerson.castro.r@gmail.com>
2022-03-01 15:46:58 -0500

    print diag

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,9 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_chessboard - this function gets the legth in bytes
+ * print_diagsums - this function gets the legth in bytes
  *
  * @a: string
+ * @size: string
  *
  */
 void print_diagsums(int *a, int size)
@@ -12,16 +13,14 @@ void print_diagsums(int *a, int size)
 	int x = 0;
 	int x1 = 0;
 
-	while (i <= (size * size) - 1)
+	for (i = 0; i <= (size * size) - 1; i += size + 1)
 	{
 		x += a[i];
-		i += size + 1;
 	}
 	i = size * (size - 1);
-	while (i >= 0)
+	for (i = size * (size - 1); i >= 0; i-= size -1)
 	{
 		x1 += a[i];
-		i -= size - 1;
 	}
 	printf("%d, %d\n", x, x1);
 }
