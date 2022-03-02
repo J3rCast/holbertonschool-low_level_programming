a4cabe4649342356179156b9f44748310afd0307
J3rCast <jerson.castro.r@gmail.com>
2022-03-02 14:15:47 -0500

    strlen recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 /**
- * _strlen_recursion(char *s) - prints a string
+ * _strlen_recursion - prints a string
  *
  * @s: source string
+ *
+ * return: len or 0
  */
 
 int _strlen_recursion(char *s)
 {
 	if (!s || !*s)
 	{
-		return(0);
+		return (0);
 	}
 
 	return (1 + _strlen_recursion(s + 1));
