a37c6dd73afff5c9bd1cd9d5c47c6fb573bd335b
J3rCast <jerson.castro.r@gmail.com>
2022-03-02 17:56:20 -0500

    sqrt recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,12 +9,14 @@
 
 int _sqrt_recursion(int n)
 {
-	int tmp = 2;
+	int tmp = 3;
 
 	if (tmp == n)
 		return (-1);
 	if ((n % tmp) == 0)
 		return (tmp);
+	if ((n % tmp) >= 1)
+		return (1);
 
 	return (_sqrt_recursion(n % (tmp + 1)));
 }
