a20985cafea6c560091725de899b56c9887fc605
J3rCast <jerson.castro.r@gmail.com>
2022-03-02 14:27:46 -0500

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * factorial - prints a string
+ *
+ * @n: sourc number
+ *
+ * Return: len or 0
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+
+	return (n * _strlen_recursion(n - 1));
+}
