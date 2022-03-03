59aff86b8de0f52b4ad209998d7b7c5d9435a364
J3rCast <jerson.castro.r@gmail.com>
2022-03-03 09:31:54 -0500

    wildcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * wildcmp - this function compares two strings
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: result
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (s1 == * && s2 == *)
+		s1 == s2;
+	if (!s1 || !s2 || !*s1 || !*s2)
+		return (0);
+	if (s1 != s2)
+		return (0);
+	if (s1 == s2 && s1 == '\0' && s2 == '\0')
+		return (1);
+
+	return (wildcmp());
+}
