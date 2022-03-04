4349ae55aee5589ab676e3d3681d9453aa23c3b1
J3rCast <jerson.castro.r@gmail.com>
2022-03-04 14:31:48 -0500

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,10 +11,31 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	int result = 0;
+	int in;
+	int sum = 0;
 
-	for (i = 1; i < argc; i++)
+	if (argc > 1)
+	{
+		while (in < argc)
+		{
+			sum = sum + atoi(argv[in]);
+			in = in + 1;
+		}
+		printf("%d\n", sum);
+	}
+	else if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+
+	/*for (i = 1; i < argc; i++)
 	{
 		if (*argv[i] >= '0' && *argv[i] <= '9')
 		{
@@ -22,10 +43,11 @@ int main(int argc, char *argv[])
 		}
 		else
 		{
-			/*printf("Error\n");*/
+			printf("Error\n");
 			return (1);
 		}
 	}
 	printf("%d\n", result);
-	return (0);
+	return (0);*/
+
 }
