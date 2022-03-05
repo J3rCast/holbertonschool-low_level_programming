4bad57713efc2181aa10362ac216e59b62d5212f
J3rCast <jerson.castro.r@gmail.com>
2022-03-05 15:09:33 -0500

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -14,33 +14,37 @@ int main(int argc, char *argv[])
 	int coins[] = { 25, 10, 5, 2, 1 };
 	int i;
 	int x = atoi(argv[1]);
-	/*int amount = 0;*/
 	int value = 0;
 
-	if (argc != 2)
+	if (*argv[1] <= 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	if (x > 0 && argc == 2)
+	{
+		for (i = 0; x > 0; i++)
+		{
+			if (coins[i] <= x)
+			{
+				x = x - coins[i];
+				i--;
+				value++;
+			}
+			else if (coins[i] == x)
+			{
+				printf("1\n");
+				return (0);
+			}
+		}
+		printf("%d\n", value);
+		return (0);
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-	if (*argv[1] <= 0)
-	{
-		printf("0\n");
-	}
-	for (i = 0; x > 0; i++)
-	{
-		if (coins[i] <= x)
-		{
-			/*amount += coins[i];*/
-			x = x - coins[i];
-			value++;
-		}
-		else if (coins[i] == x)
-		{
-			printf("1\n");
-			return(0);
-		}
-	}
-	printf("%d\n", value);
 
 	return (0);
 }
