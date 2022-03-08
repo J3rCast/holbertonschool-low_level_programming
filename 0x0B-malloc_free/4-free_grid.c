f15a566125930baf9a82a1a814e9a143ed2acc64
J3rCast <jerson.castro.r@gmail.com>
2022-03-08 12:14:29 -0500

    freed grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - this function concatenate two strings
+ *
+ * @width: width
+ * @height: height
+ *
+ * Return: int
+ */
+
+void free_grid(int **grid, int height);
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (array[i] == NULL)
+		{
+			for (; i >= 0; i--)
+			{
+				free(array[i]);
+			}
+			free(array);
+			return (NULL);
+		}
+	}
+}
