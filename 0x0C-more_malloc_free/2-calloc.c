f70d7338ad609580a12bcc161f78c85505633492
J3rCast <jerson.castro.r@gmail.com>
2022-03-11 14:40:05 -0500

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,26 +2,6 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _memset - this function fills n bytes
- *
- * @s: s value of the memory area that has to get fills
- * @b: constat
- * @n: fills n bytes of memory area
- *
- * Return: s
- */
-
-char *_memset(char *s, char b, unsigned int n)
-{
-	char *p = s;
-
-	while (n-- > 0)
-	{
-		*p++ = b;
-	}
-	return (s);
-}
-/**
  * _calloc - allocates memory for an array, using malloc
  *
  * @nmemb: elements
@@ -33,11 +13,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *m;
 
-	if (nmemb == 0 || size == 0)
+	if (nmemb == 0)
+		return (NULL);
+	if (size == 0)
 		return (NULL);
 	m = malloc(sizeof(size) * nmemb);
 	if (m == NULL)
 		return (NULL);
-	_memset(m, 0x00, size);
-	return (m);
+	return (m)
 }
