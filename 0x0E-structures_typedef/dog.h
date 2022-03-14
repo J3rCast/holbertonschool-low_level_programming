fc02fb7adeddf4dc502e31fe3eacb66fd7c249b4
J3rCast <jerson.castro.r@gmail.com>
2022-03-14 13:06:37 -0500

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _SAMPLE_HEADER_H_
+#define _SAMPLE_HEADER_H_
+/**
+ * struct dog - struct of dog data
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
