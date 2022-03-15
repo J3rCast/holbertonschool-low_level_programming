993b22a18cb8895253c7b040d643709109b3215a
J3rCast <jerson.castro.r@gmail.com>
2022-03-15 13:11:33 -0500

    new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * new_dog - this funcion prints dog nfo
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * dog - struct name
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (new_dog == NULL)
+		return;
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+}
