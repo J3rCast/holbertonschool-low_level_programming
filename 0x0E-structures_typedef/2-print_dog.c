4e24518fdc1ab0b992414d129076bb3848ecd421
J3rCast <jerson.castro.r@gmail.com>
2022-03-15 12:20:22 -0500

    update print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -2,11 +2,8 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
- * init_dog - this funcion initialize a variable
+ * print_dog - this funcion prints dog info
  * @d: type struct
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
  * dog - struct name
  */
 void print_dog(struct dog *d)
