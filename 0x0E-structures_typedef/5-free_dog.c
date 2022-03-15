b25d33f257e16f7b7d9ed60b03963fba053c27d4
J3rCast <jerson.castro.r@gmail.com>
2022-03-15 15:16:31 -0500

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_dog - this funcion frees dogs
+ * @d: dog name
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+	free(d->name);
+	free(d->age);
+	free(d->owner);
+}
