dcf5a95c91b6f9b3bd9e15856c8732b5197dd85c
J3rCast <jerson.castro.r@gmail.com>
2022-03-15 12:47:09 -0500

    update print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,18 +10,15 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-
-	if (d->name == NULL)
+	if (!d->name)
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
-
 	if (d->age < 0)
 		printf("Age: (nil)\n");
 	else
 		printf("Age: %.6f\n", d->age);
-
-	if (d->owner == NULL)
+	if (!d->owner)
 		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
