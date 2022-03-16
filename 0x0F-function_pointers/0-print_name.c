d7ccb1850cd0c298d879f40a0610c2c0ad4f5328
J3rCast <jerson.castro.r@gmail.com>
2022-03-16 13:53:28 -0500

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - this fucntion prints a name
+ * @name: name to print
+ * @f: pointer function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	void (*n)(char *) = f;
+	(*n)(name);
+}
