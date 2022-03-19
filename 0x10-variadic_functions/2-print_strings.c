999f78fc9e0059a4676c97751ffa1c8054501e9e
J3rCast <jerson.castro.r@gmail.com>
2022-03-19 16:07:13 -0500

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (argString != NULL)
 			printf("%s%s", argString, separator);
 		else
-			printf("(nil)");
+			printf("(nil)%s", separator);
 	}
 	printf("%s", va_arg(ap, char *));
 	printf("\n");
