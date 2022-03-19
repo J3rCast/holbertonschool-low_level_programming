5fc97636e52ad6525b0ce10c9bac1f61466ffbf5
J3rCast <jerson.castro.r@gmail.com>
2022-03-19 14:09:00 -0500

    update print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,10 +16,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 1; i < n; i++)
 	{
 		if (separator != NULL)
-			printf("%ld%s", va_arg(ap, long int), separator);
+			printf("%d%s", va_arg(ap, int), separator);
 	}
 	if (separator != NULL)
-		printf("%ld", va_arg(ap, long int));
-	printf("\n");
+		printf("%d", va_arg(ap, int));
 	va_end(ap);
+	printf("\n");
 }
