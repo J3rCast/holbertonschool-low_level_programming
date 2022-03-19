d73e10175aa820c56f0a96df0b47ac4c342ab6ec
J3rCast <jerson.castro.r@gmail.com>
2022-03-19 16:05:34 -0500

    print strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - this function print all the integers passed
+ * @separator: separator char
+ * @n: number of arguments passed
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	char *argString;
+	unsigned int i;
+
+	va_start(ap, n);
+	for (i = 1; i < n; i++)
+	{
+		argString = va_arg(ap, char *);
+
+		if (argString != NULL)
+			printf("%s%s", argString, separator);
+		else
+			printf("(nil)");
+	}
+	printf("%s", va_arg(ap, char *));
+	printf("\n");
+	va_end(ap);
+}
