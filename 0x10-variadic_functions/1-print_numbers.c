fe2706a4624f93416451004070e381ffe9e2dfb0
J3rCast <jerson.castro.r@gmail.com>
2022-03-19 13:49:38 -0500

    print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - this function sum all the arguments passed
+ * @separator: separator char
+ * @n: last argument fixed
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	if (n == 0)
+		return;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		if (separator != NULL)
+			printf("%u%s ", va_arg(ap, const unsigned int), separator);
+	}
+	printf("\n");
+	va_end(ap);
+}
