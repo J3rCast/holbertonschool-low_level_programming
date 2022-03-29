b5e5b3e5e315ac0b605235325cda072eee20bba4
J3rCast <jerson.castro.r@gmail.com>
2022-03-29 13:46:22 -0500

    update first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-void myStartupFun (void) __attribute__ ((constructor));
+void myStartupFun(void) __attribute__ ((constructor));
 /**
  * myStartupFun - this function executes before main function
  */
-void myStartupFun (void)
+void myStartupFun(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
