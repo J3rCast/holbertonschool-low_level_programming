9f951c5ef03bdeffa254ed4fab43a6478f1023d2
J3rCast <jerson.castro.r@gmail.com>
2022-04-01 15:54:55 -0500

    update insert node int

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	newNode = (listint_t *) malloc(sizeof(listint_t));
 	if (newNode == NULL)
 		return (NULL);
-	if (!head || !*head)
+	if (!head)
 		return (NULL);
 	temp = (*head);
 	if (idx > 0)
