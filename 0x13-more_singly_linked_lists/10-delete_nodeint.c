60ce8908c680b022417784c41fbaaba784cb3f32
J3rCast <jerson.castro.r@gmail.com>
2022-04-02 10:44:53 -0500

    delete nodeint

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - this function removes a specific node
+ * @head: pointer to list head
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *temp;
+	listint_t *previous;
+
+	if (!(*head))
+		return (-1);
+
+	temp = *head;
+	if (index > 0)
+	{
+		while (i < (index - 1))
+		{
+			previous = temp;
+			if (temp->next == NULL)
+				return (-1);
+			temp = temp->next;
+			i++;
+		}
+	previous->next = temp->next;
+	}
+
+	(*head) = (*head)->next;
+	free(temp);
+	return (0);
+}
