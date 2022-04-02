ec9a61b6e4b6aea10ad1c32e72b16480711ec802
J3rCast <jerson.castro.r@gmail.com>
2022-04-02 11:53:08 -0500

    update delete nodeint

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -28,7 +28,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	previous->next = temp->next;
 	}
 
-	*head = (*head)->next;
+	if (index == 0)
+		*head = (*head)->next;
 	free(temp);
 	return (1);
 }
