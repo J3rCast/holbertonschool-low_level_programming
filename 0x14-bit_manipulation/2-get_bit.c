733138a00ff536477c481be1cd96468673b6fd39
J3rCast <jerson.castro.r@gmail.com>
2022-04-04 15:53:29 -0500

    get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * get_bit - this function returns a bit of at a given index
+ * @index: index
+ * Return: bit of given index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int bit;
+
+	if (index > n)
+		return (-1);
+
+	bit = n >> index;
+
+	return (bit & 1);
+}
