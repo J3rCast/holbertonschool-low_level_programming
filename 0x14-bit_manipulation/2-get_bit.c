83b1aaa3dd72b83f7a47efef55ea564ea485b4b8
J3rCast <jerson.castro.r@gmail.com>
2022-04-04 15:56:11 -0500

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,14 +2,15 @@
 /**
  * get_bit - this function returns a bit of at a given index
  * @index: index
+ * @n: number given
  * Return: bit of given index
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int bit;
 
-	if (index > n)
-		return (-1);
+	/*if (index > n)*/
+		/*return (-1);*/
 
 	bit = n >> index;
 
