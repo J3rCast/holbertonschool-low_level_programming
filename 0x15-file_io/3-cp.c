60005492f073247415fa7aeec46d3474000b2923
J3rCast <jerson.castro.r@gmail.com>
2022-04-05 16:23:28 -0500

    update my cp program

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,15 @@
 #include "main.h"
+void closeFd(int fd)
+{
+	int i;
+
+	i = close(fd);
+	if (i < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", i);
+		exit(100);
+	}
+}
 /**
  * main - copy file
  * @argc: number of arguments
@@ -7,42 +18,39 @@
  */
 int main(int argc, char *argv[])
 {
-	int oRet2 = 0, oRet = 0, rRet = 0, wRet = 0, cRet = 0, cRet2 = 0;
-	char text[4080];
+	int oRet2 = 0, oRet = 0, rRet = 0, wRet = 0;
+	char *text;
 
 	if (argc != 3)
 	{
 		write(STDERR_FILENO, "Usage: cp file_from file_to\n", 28);
 		exit(97);
 	}
+	text = malloc(sizeof(char) * 1024);
 	oRet = open(argv[1], O_RDONLY);
 	if (oRet < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		free(text);
 		exit(98);
 	}
 	rRet = read(oRet, text, 1024);
-	cRet = close(oRet);
+	closeFd(oRet);
 	oRet2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (oRet2 < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		free(text);
 		exit(99);
 	}
 	wRet = write(oRet, text, rRet);
 	text[1024] = '\0';
-	cRet2 = close(oRet2);
-	if (cRet < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", cRet);
-		exit(100);
-	}
-	if (cRet2 < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", cRet2);
-		exit(100);
-	}
+	closeFd(oRet2);
 	if (wRet == -1 || oRet == -1 || rRet == -1)
+	{
+		free(text);
 		return (-1);
-	return (1);
+	}
+	free(text);
+	return (0);
 }
