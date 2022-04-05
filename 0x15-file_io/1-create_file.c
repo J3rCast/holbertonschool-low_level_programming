f0d4c9c1607aeda251d2054d580b563f604bf2ce
J3rCast <jerson.castro.r@gmail.com>
2022-04-05 13:53:16 -0500

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,10 +18,10 @@ int _strlen(char *s)
 	return (i - 1);
 }
 /**
- * append_text_to_file - appends text at the end of a file
+ * create_file - creates a file
  * @filename: is the name of the created file
  * @text_content: content of the file
- * Return: number of letters it could read and print
+ * Return: 1 if success and -1 if fails
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -30,8 +30,8 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	oRet = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600); /* first open the file */
-
+	/*first open the file*/
+	oRet = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (text_content != NULL)
 		/* and finally writte it  in stdout*/
 		wRet = write(oRet, text_content, _strlen(text_content));
