936e1a049af46639adb6d6d07697fc95c32e1030
J3rCast <jerson.castro.r@gmail.com>
2022-05-11 14:47:32 -0500

    betty fix

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,10 +1,7 @@
 #include "lists.h"
 /**
- * add_dnodeint - add a new node at the beggining of the list
+ * free_dlistint - add a new node at the beggining of the list
  * @head: head of the list
- * @n: data of the new node
- *
- * Return: addres of the new element
  */
 void free_dlistint(dlistint_t *head)
 {
