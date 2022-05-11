64012c73703f3fa70617c1dc84fe01c2f44dc78b
J3rCast <jerson.castro.r@gmail.com>
2022-05-11 13:12:08 -0500

    betty fixes

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,8 +1,15 @@
 #include "lists.h"
+/**
+ * print_dlistint - print all elements of a linked list
+ * @h: head of the list
+ *
+ * Return: number of elements
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t elementsNumber = 0;
 	const dlistint_t *temp;
+
 	temp = h;
 
 	if (h == NULL)
