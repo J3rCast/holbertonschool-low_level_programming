8db36584a0f4c11270e1930ed14614ccf3b0c90f
J3rCast <jerson.castro.r@gmail.com>
2022-06-06 14:10:02 -0500

    hash table get added

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,7 +12,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int i = 0;
 	hash_node_t *temp = NULL;
 
-	if (!key)
+	if (!key || !*key || !ht)
 		return (NULL);
 
 	for (i = 0; i < ht->size; i++)
