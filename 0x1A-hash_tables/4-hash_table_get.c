3b65302bceb62e6f6a78f96c93dc4e592832be91
J3rCast <jerson.castro.r@gmail.com>
2022-06-06 14:05:31 -0500

    hash table get added

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - get the value of an index
+ *
+ * @ht: hash table
+ * @key: key of the value that we need
+ *
+ * Return: value
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int i = 0;
+	hash_node_t *temp = NULL;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			temp = ht->array[i];
+			while (temp)
+			{
+				if (strcmp(key, temp->key) == 0)
+				{
+					return (temp->value);
+				}
+				temp = temp->next;
+			}
+		}
+	}
+	return (NULL);
+}
