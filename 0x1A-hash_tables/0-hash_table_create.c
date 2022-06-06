b6c815237fa4bfb69b03b0ce9f4ea330269cc68c
J3rCast <jerson.castro.r@gmail.com>
2022-06-06 09:31:51 -0500

    first task added

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	newT->array = malloc(sizeof(hash_node_t *) * size);
 	if (newT->array == NULL)
 	{
-		free (newT);
+		free(newT);
 		return (NULL);
 	}
 
